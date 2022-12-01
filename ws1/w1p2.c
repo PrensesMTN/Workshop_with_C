b018cd1575d522c6f8ddf106b040d7a7cdca97cd
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2022-12-01 19:25:38 +0300

    w1p2 editted

--- ws1/w1p2.c
+++ ws1/w1p2.c
@@ -3,27 +3,26 @@
 void main()  
 
 {   int percent_sign =37;
-    int duo_quote = 34;
-    int tab = 9;
+    
     char first_name[] = "Prenses";
     char last_name[] = "Metin";
     unsigned long int studentID = 2166021001;
     printf("Workshop 1 Part-2\n");
-    printf("====================\n\n");
+    printf("=================\n\n");
     printf("Using tab specifiers...\n");
-    printf("%cTab-1<t>Tab-2<t>Tab-3\n",tab);
-    printf("%c-----<t>=====<t>-----\n\n",tab);
+    printf("\tTab-1\tTab-2\tTab-3\n");
+    printf("\t-----\t=====\t-----\n\n");
     printf("\\ this is a back-slash character!\n");
-    printf("%c this is a percent sign character!\n",percent_sign);
+    printf("%% this is a percent sign character!\n");
     printf("\""" this is a double-quote character!\n\n");
     printf("My favourite quotes are:\n");
-    printf("1.%c\"Skill is only developed by hours and hours of work.\" \n",tab);
-    printf("%c%c%c%c%c%c-Usain Bolt\n",tab,tab,tab,tab,tab,tab);
-    printf("2.%c\"It's not about having time. It's about making time.\" \n" );
-    printf("%c%c%c%c%c%c-unknown\n",tab,tab,tab,tab,tab,tab);
-    printf("3.%c\"All of us do not have equal talent. But, all of us\"\n");
-    printf("%chave an equal opportunity to develop our talents.\"\n");
-    printf("%c%c%c%c%c%c-A.P.J. Abdul Kalam",tab,tab,tab,tab,tab,tab);
+    printf("1.\t\"Skill is only developed by hours and hours of work.\" \n");
+    printf("\t\t\t\t\t\t-Usain Bolt\n");
+    printf("2.\t\"It's not about having time. It's about making time.\" \n" );
+    printf("\t\t\t\t\t\t-unknown\n");
+    printf("3.\t\"All of us do not have equal talent. But, all of us\"\n");
+    printf("\thave an equal opportunity to develop our talents.\"\n");
+    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam");
 
 }
 
