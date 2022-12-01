dfa8c87d2151d66d5e67635752e28b5d013c5d25
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2022-12-01 18:53:14 +0300

    fixing output of prints

--- ws1/w1p2.c
+++ ws1/w1p2.c
@@ -9,13 +9,13 @@ void main()
     char last_name[] = "Metin";
     unsigned long int studentID = 2166021001;
     printf("Workshop 1 Part-2\n");
-    printf("====================\n");
+    printf("====================\n\n");
     printf("Using tab specifiers...\n");
     printf("%cTab-1<t>Tab-2<t>Tab-3\n",tab);
     printf("%c-----<t>=====<t>-----\n\n",tab);
     printf("\\ this is a back-slash character!\n");
     printf("%c this is a percent sign character!\n",percent_sign);
-    printf("\'\""" this is a double-quote character!\n");
+    printf("\'\""" this is a double-quote character!\n\n");
     printf("My favourite quotes are:\n");
     printf("1.%c\"Skill is only developed by hours and hours of work.\" \n",tab);
     printf("%c%c%c%c%c%c-Usain Bolt\n",tab,tab,tab,tab,tab,tab);
