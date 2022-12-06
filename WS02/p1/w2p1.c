a7f5d3b33ccb1237976fd1155f9a2746592d0931
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2022-12-06 09:45:10 +0300

    ws1 names changed as WS1 and WS02 added

---
+++ WS02/p1/w2p1.c
@@ -0,0 +1,18 @@
+/*
+*****************************************************************************
+                          Workshop - #2 (P1)
+
+*****************************************************************************
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+    const double TAX = 0.13;
+    const char patSize = 'S';
+
+    return 0;
+}
---
+++ WS02/p2/w2p2.c
@@ -0,0 +1,19 @@
+/*
+*****************************************************************************
+                          Workshop - #2 (P2)
+
+
+*****************************************************************************
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+    const double TAX = 0.13;
+    const char patSize = 'S', salSize = 'M', tomSize = 'L';
+
+    return 0;
+}
