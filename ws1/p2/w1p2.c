30e40f09e676588578c3ea8b1b472c083c220147
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2022-12-06 09:29:19 +0300

    Update w1p2.c

--- ws1/p2/w1p2.c
+++ ws1/p2/w1p2.c
@@ -21,7 +21,7 @@ void main()
     printf("2.\t\"It's not about having time. It's about making time.\" \n" );
     printf("\t\t\t\t\t\t-unknown\n");
     printf("3.\t\"All of us do not have equal talent. But, all of us\"\n");
-    printf("\thave an equal opportunity to develop our talents.\"\n");
+    printf("\thave an equal opportunity to develop our talents.\n");
     printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam");
 
 }
